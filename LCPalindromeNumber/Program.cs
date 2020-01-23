d8c9ea70e1db232e09feadfd74a19e56a0f13c2a
jeremy_plott <plott.j@pg.com>
2020-01-23 16:02:55 -0500

    Add project files.

---
+++ LCPalindromeNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace LCPalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1001;
+
+            Console.WriteLine(CheckPalindrome(x));
+
+            bool CheckPalindrome(int number)
+            {
+                var numberString = number.ToString();
+
+                int stringLength = numberString.Length;
+
+                var firstHalf  = string.Empty;
+                var secondHalf = string.Empty;
+
+                if( stringLength % 2 == 0)
+                {
+                    firstHalf  = numberString.Substring(0, stringLength / 2);
+                    secondHalf = numberString.Substring(stringLength / 2);
+                }
+                else
+                {
+                    firstHalf  = numberString.Substring(0, stringLength / 2);
+                    secondHalf = numberString.Substring((stringLength / 2) + 1);
+                }
+
+                var mirroredHalf = string.Concat(secondHalf.Reverse());
+
+                return (firstHalf.Equals(mirroredHalf));
+            }
+        }
+    }
+}
